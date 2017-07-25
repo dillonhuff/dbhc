05abba60f58f015f6f24d5ceca7327499d8fafe3
Dillon Huff <dillonhuff@gmail.com>
2017-07-24 17:21:54 -0700

    Removing min_e_impl structure as well

--- include/dbhc/algorithm.h
+++ include/dbhc/algorithm.h
@@ -586,53 +586,23 @@ namespace dbhc {
     }
     
   };
-  
-  template<typename I>
-  class min_e_impl<std::set<I> > {
-  public:
-    typedef I result_type;
 
-    template<typename F>
-    static
-    result_type apply(const std::set<I>& e, F f) {
-      return *min_element(begin(e), end(e),
-			  [f](const I& l, const I& r) { return f(l) < f(r); });
-    }
+  template<typename T, typename F>
+  T min_e(const std::set<T>& e, F f) {
+    return *min_element(begin(e), end(e),
+			[f](const T& l, const T& r) { return f(l) < f(r); });
+  }
 
-    static
-    result_type apply(const std::set<I>& e) {
-      return *min_element(begin(e), end(e));
-    }
-    
-  };
+  template<typename T>
+  T min_e(const std::set<T>& e) {
+    return *min_element(begin(e), end(e));
+  }
   
   template<typename A, typename F>
   typename min_e_impl<A>::result_type
   min_e(const A& e, F f) {
     return min_e_impl<A>::apply(e, f);
   }
-
-  template<typename A>
-  class max_e_impl;
-
-  // template<typename I>
-  // class max_e_impl<std::vector<I> > {
-  // public:
-  //   typedef I result_type;
-
-  //   template<typename F>
-  //   static
-  //   result_type apply(const std::vector<I>& e, F f) {
-  //     return *max_element(begin(e), end(e),
-  // 			  [f](const I& l, const I& r) { return f(l) < f(r); });
-  //   }
-
-  //   static
-  //   result_type apply(const std::vector<I>& e) {
-  //     return *max_element(begin(e), end(e));
-  //   }
-    
-  // };
 
   template<typename T, typename F>
   T max_e(const std::vector<T>& e, F f) {
@@ -644,51 +614,29 @@ namespace dbhc {
   T max_e(const std::vector<T>& e) {
     return *max_element(begin(e), end(e));
   }
-  
-  template<typename I>
-  class max_e_impl<std::deque<I> > {
-  public:
-    typedef I result_type;
 
-    template<typename F>
-    static
-    result_type apply(const std::deque<I>& e, F f) {
-      return *max_element(begin(e), end(e),
-			  [f](const I& l, const I& r) { return f(l) < f(r); });
-    }
-
-    static
-    result_type apply(const std::deque<I>& e) {
-      return *max_element(begin(e), end(e));
-    }
-    
-  };
-  
-  template<typename I>
-  class max_e_impl<std::set<I> > {
-  public:
-    typedef I result_type;
-
-    template<typename F>
-    static
-    result_type apply(const std::set<I>& e, F f) {
-      return *max_element(begin(e), end(e),
-			  [f](const I& l, const I& r) { return f(l) < f(r); });
-    }
-
-    static
-    result_type apply(const std::set<I>& e) {
-      return *max_element(begin(e), end(e));
-    }
-    
-  };
-  
-  template<typename A, typename F>
-  typename max_e_impl<A>::result_type
-  max_e(const A& e, F f) {
-    return max_e_impl<A>::apply(e, f);
+  template<typename T, typename F>
+  T max_e(const std::deque<T>& e, F f) {
+    return *max_element(begin(e), end(e),
+			[f](const T& l, const T& r) { return f(l) < f(r); });
   }
 
+  template<typename T>
+  T max_e(const std::deque<T>& e) {
+    return *max_element(begin(e), end(e));
+  }
+
+  template<typename T, typename F>
+  T max_e(const std::set<T>& e, F f) {
+    return *max_element(begin(e), end(e),
+			[f](const T& l, const T& r) { return f(l) < f(r); });
+  }
+
+  template<typename T>
+  T max_e(const std::set<T>& e) {
+    return *max_element(begin(e), end(e));
+  }
+  
   template<typename T, typename F>
   T max_e(const std::unordered_set<T>& e, F f) {
     return *max_element(begin(e), end(e),
@@ -759,12 +707,6 @@ namespace dbhc {
   typename min_e_impl<A>::result_type
   min_e(const A& e) {
     return min_e_impl<A>::apply(e);
-  }
-
-  template<typename A>
-  typename max_e_impl<A>::result_type
-  max_e(const A& e) {
-    return max_e_impl<A>::apply(e);
   }
 
   template<typename T, typename F>
